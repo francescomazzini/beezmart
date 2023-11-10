a77e02bbe05c792abe34002bc611d98cd9b1e246
Alessandro Ercolani <alesercolani@unibz.it>
2023-11-11 00:08:57 +0100

    login page

--- Beezmart/app/UI/LoginPage.tsx
+++ Beezmart/app/UI/LoginPage.tsx
@@ -1,14 +1,29 @@
 import { Text, View } from "react-native";
+import { InputText } from "./components/Inputs";
 
-
-const LoginPage = ({navigation}:{navigation:any}) : JSX.Element => {
-  
-    return (
-      <View>
-        <Text style={{color: "black"}}>AOO</Text>
+const LoginPage = ({ navigation }: { navigation: any }): JSX.Element => {
+  return (
+    <View style={{ backgroundColor: "#27241f" }}>
+      <View
+        style={{
+          backgroundColor: "#27241f",
+          height: "40%",
+        }}
+      ></View>
+      <View
+        style={{
+          height: "60%",
+          justifyContent: "center",
+          gap: 20,
+          marginHorizontal: 66,
+          backgroundColor: "#27241f",
+        }}
+      >
+        <InputText label="Email" />
+        <InputText label="Password" />
       </View>
-    );
-  }
-
+    </View>
+  );
+};
 
 export default LoginPage;
---
+++ Beezmart/app/UI/components/Inputs.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { TextInput } from "react-native-paper";
+
+const InputText = ({ label }: { label: string }) => {
+  const [text, setText] = React.useState("");
+
+  return (
+    <TextInput
+      style={{
+        width: "100%",
+
+        borderBottomLeftRadius: 50,
+        borderBottomRightRadius: 50,
+        borderTopRightRadius: 50,
+        borderTopLeftRadius: 50,
+        borderWidth: 1,
+        borderColor: "gray",
+      }}
+      label={label}
+      value={text}
+      onChangeText={(text) => setText(text)}
+    />
+  );
+};
+
+export { InputText };
