c6001f2991ede64dc1a86cb541d1f51f2b60fef5
Alessandro Ercolani <alesercolani@unibz.it>
2023-11-11 05:25:13 +0100

    added change of color

--- Beezmart/app/UI/TransferPage.tsx
+++ Beezmart/app/UI/TransferPage.tsx
@@ -1,6 +1,6 @@
 import { Image, Text, TouchableOpacity, View } from "react-native";
 import { AuthContext } from "../core/useUser";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useWallet } from "../core/useWallet";
 import React from "react";
 import { Divider } from "react-native-paper";
@@ -8,6 +8,96 @@ import { Divider } from "react-native-paper";
 import { IconFoto, InputButton, InputText } from "./components/Inputs";
 
 const TransferPage = ({ navigation }: { navigation: any }): JSX.Element => {
+  const [imageSource, setImageSource] = useState(
+    require("./assets/images/1.png")
+  );
+
+  const handleImagePress1 = () => {
+    // Change the image source based on some condition or logic
+    // For demonstration, let's toggle between two images
+    const newImageSource =
+      imageSource === require("./assets/images/1.png")
+        ? require("./assets/images/1_Giallo-20.png")
+        : require("./assets/images/1.png");
+
+    setImageSource(newImageSource);
+  };
+
+  const [imageSource2, setImageSource2] = useState(
+    require("./assets/images/5.png")
+  );
+
+  const handleImagePress2 = () => {
+    // Change the image source based on some condition or logic
+    // For demonstration, let's toggle between two images
+    const newImageSource2 =
+      imageSource2 === require("./assets/images/5.png")
+        ? require("./assets/images/5_Giallo-21.png")
+        : require("./assets/images/5.png");
+
+    setImageSource2(newImageSource2);
+  };
+
+  const [imageSource3, setImageSource3] = useState(
+    require("./assets/images/25.png")
+  );
+
+  const handleImagePress3 = () => {
+    // Change the image source based on some condition or logic
+    // For demonstration, let's toggle between two images
+    const newImageSource3 =
+      imageSource3 === require("./assets/images/25.png")
+        ? require("./assets/images/25_Giallo-23.png")
+        : require("./assets/images/25.png");
+
+    setImageSource3(newImageSource3);
+  };
+
+  const [imageSource4, setImageSource4] = useState(
+    require("./assets/images/50.png")
+  );
+
+  const handleImagePress4 = () => {
+    // Change the image source based on some condition or logic
+    // For demonstration, let's toggle between two images
+    const newImageSource4 =
+      imageSource4 === require("./assets/images/50.png")
+        ? require("./assets/images/50_Giallo-24.png")
+        : require("./assets/images/50.png");
+
+    setImageSource4(newImageSource4);
+  };
+
+  const [imageSource5, setImageSource5] = useState(
+    require("./assets/images/10.png")
+  );
+
+  const handleImagePress5 = () => {
+    // Change the image source based on some condition or logic
+    // For demonstration, let's toggle between two images
+    const newImageSource5 =
+      imageSource5 === require("./assets/images/10.png")
+        ? require("./assets/images/10_Giallo-22.png")
+        : require("./assets/images/10.png");
+
+    setImageSource5(newImageSource5);
+  };
+
+  const [imageSource6, setImageSource6] = useState(
+    require("./assets/images/100.png")
+  );
+
+  const handleImagePress6 = () => {
+    // Change the image source based on some condition or logic
+    // For demonstration, let's toggle between two images
+    const newImageSource6 =
+      imageSource6 === require("./assets/images/100.png")
+        ? require("./assets/images/100_Giallo-25.png")
+        : require("./assets/images/100.png");
+
+    setImageSource6(newImageSource6);
+  };
+
   return (
     <View style={{ height: "100%", backgroundColor: "#27241f" }}>
       <View
@@ -26,32 +116,20 @@ const TransferPage = ({ navigation }: { navigation: any }): JSX.Element => {
       <Divider />
       <View style={{ height: "63%", flexDirection: "row", marginTop: 20 }}>
         <View style={{ height: "100%" }}>
-          <TouchableOpacity>
-            <Image
-              style={{ width: 130, height: 130 }}
-              source={require("./assets/images/1.png")}
-            />
+          <TouchableOpacity onPress={handleImagePress1}>
+            <Image style={{ width: 130, height: 130 }} source={imageSource} />
           </TouchableOpacity>
-          <TouchableOpacity>
-            <Image
-              style={{ width: 130, height: 130 }}
-              source={require("./assets/images/25.png")}
-            />
+          <TouchableOpacity onPress={handleImagePress3}>
+            <Image style={{ width: 130, height: 130 }} source={imageSource3} />
           </TouchableOpacity>
         </View>
         <Divider />
         <View style={{ height: "100%" }}>
-          <TouchableOpacity>
-            <Image
-              style={{ width: 130, height: 130 }}
-              source={require("./assets/images/5.png")}
-            />
+          <TouchableOpacity onPress={handleImagePress2}>
+            <Image style={{ width: 130, height: 130 }} source={imageSource2} />
           </TouchableOpacity>
-          <TouchableOpacity>
-            <Image
-              style={{ width: 130, height: 130 }}
-              source={require("./assets/images/50.png")}
-            />
+          <TouchableOpacity onPress={handleImagePress4}>
+            <Image style={{ width: 130, height: 130 }} source={imageSource4} />
           </TouchableOpacity>
           <TouchableOpacity>
             <Image
@@ -62,17 +140,11 @@ const TransferPage = ({ navigation }: { navigation: any }): JSX.Element => {
         </View>
         <Divider />
         <View style={{ height: "100%" }}>
-          <TouchableOpacity>
-            <Image
-              style={{ width: 130, height: 130 }}
-              source={require("./assets/images/10.png")}
-            />
+          <TouchableOpacity onPress={handleImagePress5}>
+            <Image style={{ width: 130, height: 130 }} source={imageSource5} />
           </TouchableOpacity>
-          <TouchableOpacity>
-            <Image
-              style={{ width: 130, height: 130 }}
-              source={require("./assets/images/100.png")}
-            />
+          <TouchableOpacity onPress={handleImagePress6}>
+            <Image style={{ width: 130, height: 130 }} source={imageSource6} />
           </TouchableOpacity>
         </View>
         <Divider />
