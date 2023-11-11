3d307c72604afa6282bceb06fa0435a3a13ac640
Francesco <francesco.mazzini432@gmail.com>
2023-11-11 01:09:16 +0100

    Added auth context
    Signed-off-by: Francesco <francesco.mazzini432@gmail.com>

---
+++ Beezmart/app/UI/HomePage.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+
+
+const HomePage = ({navigation}:{navigation:any}) : JSX.Element => {
+  
+    return (
+      <View>
+        <Text style={{color: "black"}}>AOO</Text>
+      </View>
+    );
+  }
+
+
+export default HomePage;
